403313586d97fe07619ce48002c5c06fa9c77d71
Carolina <crodriguez@iesrafaelalberti.es>
2022-10-25 14:21:15 +0200

    bien

--- codigophp/index.php
+++ codigophp/index.php
@@ -15,17 +15,16 @@ require("conecta.php");
 <body>
 <?php
     $name = $_SESSION['user'];
+    print($_SESSION['user']);
     $memes = $conn->query("Select * FROM created_memes WHERE id_user = (SELECT id FROM users WHERE name = '$name')");
-    //Select * FROM created_memes WHERE id_user = (SELECT id FROM users WHERE name = $_SESSION['user'])
     if($memes->rowCount() == 0) {
         print("<p>No tienes memes</p>");
     }
-    print($_SESSION['user']);
     print("<table class='memes'>");
     while($meme = $memes->fetchObject()) {
         print("<tr>");
         print("<td>");
-        print("<a href='borrarmeme.php?id=".$user["id"]."'><i class='fa-solid fa-trash-can'></i>");
+        print("<a href='borrarmeme.php?id="$meme->id"'><i class='fa-solid fa-trash-can'></i>");
         print("</td>");
         print("<td>");
         print($meme->name);
@@ -33,6 +32,8 @@ require("conecta.php");
         print("</tr>");
     }
     print("</table>");
+
+    print("<a href='phpinfo.php'>phpinfo()</a>")
 ?>
     <a href="phpinfo.php">phpinfo()</a>
     <a href="xdebug_info.php">xdebug_info()</a>
